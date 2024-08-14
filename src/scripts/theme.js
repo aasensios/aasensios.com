7bb8cd4b3a7574d392ca6aa95ce4d4bee55c9d0c
Alejandro Asensio <alejandro.asensio@trustyou.net>
2024-08-14 13:43:57 +0200

    apply different favicon on user dark scheme change

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,18 +1,37 @@
 function applyTheme(theme) {
-  document.body.classList.remove('latte', 'mocha');
-  document.body.classList.add(theme);
+	document.body.classList.remove("latte", "mocha");
+	document.body.classList.add(theme);
+}
+
+function applyFavicon() {
+	const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	const favicon = document.getElementById("favicon");
+	if (isDarkMode) {
+		favicon.setAttribute("href", "/favicon-dark.svg");
+	} else {
+		favicon.setAttribute("href", "/favicon-light.svg");
+	}
 }
 
 function detectColorScheme() {
-  const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  applyTheme(isDarkMode ? 'mocha' : 'latte');
+	const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	applyTheme(isDarkMode ? "mocha" : "latte");
+	applyFavicon();
 }
 
 // Listen for changes to the color scheme
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-  const newColorScheme = event.matches ? 'mocha' : 'latte';
-  applyTheme(newColorScheme);
-});
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", (event) => {
+		const newColorScheme = event.matches ? "mocha" : "latte";
+		applyTheme(newColorScheme);
+		applyFavicon();
+	});
 
 // Initial detection of color scheme
-document.addEventListener('DOMContentLoaded', detectColorScheme);
+document.addEventListener("DOMContentLoaded", detectColorScheme);
+
+// Listen for changes in color scheme
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", applyFavicon);
